657de86ed44fc81764375ee970a8012cd76ce115
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2023-01-20 11:10:46 +0100

    fix Windows problem with opening a dir, update environment.yml

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import os
 import json
+import platform
+from datetime import datetime
 
 from models.model_loader import ModelLoader
 from train.trainer_loader import TrainerLoader
@@ -12,15 +14,17 @@ from utils.misc import get_split_str
 
 import torch
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    start_time = time.time()
 
     # Parse arguments
     args = utils.arg_parser.get_args()
     # Print arguments
     utils.arg_parser.print_args(args)
 
-    device = torch.device('cuda:{}'.format(args.cuda_device) if
-            torch.cuda.is_available() and not args.disable_cuda else 'cpu')
+    device = torch.device("cuda:{}".format(args.cuda_device) if
+            torch.cuda.is_available() and not args.disable_cuda else "cpu")
 
     job_string = time.strftime("{}-{}-D%Y-%m-%d-T%H-%M-%S-G{}".format(args.model, args.dataset, args.cuda_device))
 
@@ -28,11 +32,10 @@ if __name__ == '__main__':
 
 
     # Create new checkpoint directory
-    #if not os.path.exists(job_path):
-    os.makedirs(job_path)
+    os.makedirs(job_path, exist_ok=True)
 
     # Save job arguments
-    with open(os.path.join(job_path, 'config.json'), 'w') as f:
+    with open(os.path.join(job_path, "config.json"), "w") as f:
         json.dump(vars(args), f)
 
     # Data preparation
@@ -42,7 +45,7 @@ if __name__ == '__main__':
     dataset, data_loader = data_prep.get_dataset_and_loader(split, args.pretrained_model,
             batch_size=args.batch_size, num_workers=args.num_workers)
     if args.train:
-        val_dataset, val_data_loader = data_prep.get_dataset_and_loader('val',
+        val_dataset, val_data_loader = data_prep.get_dataset_and_loader("val",
                 args.pretrained_model, batch_size=args.batch_size, num_workers=args.num_workers)
 
     # TODO: If eval + checkpoint load validation set
@@ -52,7 +55,7 @@ if __name__ == '__main__':
     print("Loading Model ...")
     ml = ModelLoader(args, dataset)
     model = getattr(ml, args.model)()
-    print(model, '\n')
+    print(model, "\n")
 
     # TODO: Remove and handle with checkpoints
     if not args.train:
@@ -67,7 +70,7 @@ if __name__ == '__main__':
         val_dataset.set_label_usage(dataset.return_labels)
 
     # Create logger
-    logger = Logger(os.path.join(job_path, 'logs'))
+    logger = Logger(os.path.join(job_path, "logs"))
 
     # Get trainer
     trainer_creator = getattr(TrainerLoader, args.model)
@@ -81,7 +84,7 @@ if __name__ == '__main__':
         print("Training ...")
     else:
         print("Evaluating ...")
-        vars(args)['num_epochs'] = 1
+        vars(args)["num_epochs"] = 1
 
 
     # Start training/evaluation
@@ -94,6 +97,12 @@ if __name__ == '__main__':
             # Eval & Checkpoint
             checkpoint_name = "ckpt-e{}".format(trainer.curr_epoch)
             checkpoint_path = os.path.join(job_path, checkpoint_name)
+        
+            file = open("evaluation.txt", "a")
+            today = datetime.now()
+            currentDateTime = today.strftime("%b-%d-%Y-%H-%M-%S")
+            file.write("%s Epoch %i:\n"%(currentDateTime, trainer.curr_epoch))
+            file.close()
 
             model.eval()
             result = evaluator.train_epoch()
@@ -103,26 +112,35 @@ if __name__ == '__main__':
                 score = result
             model.train()
 
-            logger.scalar_summary('score', score, trainer.curr_epoch)
+            logger.scalar_summary("score", score, trainer.curr_epoch)
 
             # TODO: Eval model
             # Save the models
-            checkpoint = {'epoch': trainer.curr_epoch,
-                          'max_score': max_score,
-                          'optimizer' : trainer.optimizer.state_dict()}
+            checkpoint = {"epoch": trainer.curr_epoch,
+                          "max_score": max_score,
+                          "optimizer" : trainer.optimizer.state_dict()}
             checkpoint_path += ".pth"
             torch.save(model.state_dict(), checkpoint_path)
             torch.save(checkpoint, os.path.join(job_path,
                 "training_checkpoint.pth"))
             if score > max_score:
                 max_score = score
-                link_name = "best-ckpt.pth"
+                link_name = "best-ckpt_" + job_string + ".pth"
                 link_path = os.path.join(job_path, link_name)
                 if os.path.islink(link_path):
+                    print(link_path, "is a symbolic link")
                     os.unlink(link_path)
-                dir_fd = os.open(os.path.dirname(link_path), os.O_RDONLY)
+                # Opening a directory is not possible on Windows, but that is not
+                # a problem since Windows does not need to fsync the directory in
+                # order to persist metadata.
+                # Code adapted from https://github.com/NicolasLM/bplustree/commit/97de3d04022169f4ab0abd3d2ce97e5580bdff76
+                if platform.system() == "Windows":
+                    dir_fd = None
+                else:
+                    dir_fd = os.open(os.path.dirname(link_path), os.O_RDONLY)
                 os.symlink(os.path.basename(checkpoint_path), link_name, dir_fd=dir_fd)
-                os.close(dir_fd)
+                if platform.system() != "Windows":
+                    os.close(dir_fd)
 
         else:
             result = trainer.train_epoch()
@@ -130,7 +148,10 @@ if __name__ == '__main__':
                 score = dataset.eval(result, "results")
 
 
-    if not args.train and args.model == 'sc':
-        with open('results.json', 'w') as f:
+    if not args.train and args.model == "sc":
+        with open("results.json", "w") as f:
             json.dump(result, f)
 
+    elapsed_time = time.time() - start_time
+    time_string = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+    print("\nElapsed time:", time_string)
